f7d194faae69abf13c5d207ed7c9c9b7df6bd33d
4022s <4022s@users.noreply.local>
2026-03-03 10:46:32 -0800

    fixed/added the right routes for the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,52 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home').then(m => m.Home),
+    title: 'Home'
+  },
+  {
+    path: 'objects',
+    loadComponent: () =>
+      import('./features/objects/objects-list/objects-list')
+        .then(m => m.ObjectsList),
+    title: 'Objects'
+  },
+  {
+    path: 'objects/new',
+    loadComponent: () =>
+      import('./features/objects/object-form/object-form')
+        .then(m => m.ObjectForm),
+    title: 'Create Object'
+  },
+  {
+    path: 'objects/:id',
+    loadComponent: () =>
+      import('./features/objects/object-details/object-details')
+        .then(m => m.ObjectDetails),
+    title: 'Object Details'
+  },
+  {
+    path: 'objects/:id/edit',
+    loadComponent: () =>
+      import('./features/objects/object-form/object-form')
+        .then(m => m.ObjectForm),
+    title: 'Edit Object'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login')
+        .then(m => m.Login),
+    title: 'Login'
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./shared/not-found/not-found')
+        .then(m => m.NotFound),
+    title: '404 - Not Found'
+  }
+];
